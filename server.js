c3719595bf7b976e7250196ad53374bfff255db9
kpoxo6op <kpoxo6op@gmail.com>
2023-04-17 15:19:35 +1200

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,7 @@ const { exec } = require('child_process');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/update-image' && req.method === 'POST') {
-    exec('./update_image.sh', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: \${error}`);
-        res.statusCode = 500;
-        res.end('Error updating image');
-        return;
-      }
-      console.log(`stdout: \${stdout}`);
-      console.error(`stderr: \${stderr}`);
-      res.statusCode = 200;
-      res.end('Image updated successfully');
-    });
+    console.log(`new image version released!`);
   } else {
     res.statusCode = 404;
     res.end('Not found');
